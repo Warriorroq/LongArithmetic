5326552c2694755858785fc34f446dadbeab131e
Arthur <Warriorroq@gmail.com>
2021-11-17 10:39:55 +0300

    Created projects

---
+++ LongLibrary/LongNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LongLibrary
+{
+    public struct LongNumber
+    {
+        public LongNumber(int value, int basis)
+        {
+            this.value = value;
+            this.basis = basis;
+        }
+        public int value;
+        public int basis;
+    }
+}
---
+++ LongNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LongLibrary;
+namespace LongNumbers
+{
+    public class Program
+    {
+        private Program() { }
+        private static void Main(string[] args)
+            => new Program().Execute();
+        private void Execute()
+        {
+            LongNumber longNumber = new LongNumber();
+        }
+    }
+
+}
