751981a4a551bc0b399d160afe1f09fd09556b90
Arthur <Warriorroq@gmail.com>
2022-01-18 20:14:39 +0300

    fix stash

--- LongLibTests/UnitTest1.cs
+++ LongLibTests/UnitTest1.cs
@@ -4,30 +4,6 @@ namespace LongLibTests
 {
     public class Tests
     {
-<<<<<<< Updated upstream
-        private LongNumber num1;
-        private LongNumber num2;
-        private LongNumber num3;
-        private LongNumber num4;
-        private LongNumber num5;
-        private LongNumber num6;
-        private LongNumber num7;
-        [SetUp]
-        public void Setup()
-        {
-            num1 = LongMath.GetLongNumber("932758236583249327948621932562341");
-            num2 = LongMath.GetLongNumber("65783442658212443786544326523467");
-            num3 = LongMath.GetLongNumber("-23578465723590238409823590982738");
-            num4 = LongMath.GetLongNumber("-99999999999999999999999999999999");
-            num5 = LongMath.GetLongNumber("100000000000000000000000000000000");
-            num6 = LongMath.GetLongNumber("9998");
-            num7 = LongMath.GetLongNumber("932758236583249327948621932562341");
-        }
-
-        [Test]
-        public void EqualTest()
-            =>Assert.IsTrue(num1 == num7);
-=======
 
         [Test]
         public void EqualTest()
@@ -103,6 +79,5 @@ namespace LongLibTests
             var answer = LongMath.GetLongNumber("53 971 868 264 001 217 695 157 894 248 470 650");
             Assert.IsTrue(num1 - num2 == answer);
         }
->>>>>>> Stashed changes
     }
 }
--- LongNumbers/Program.cs
+++ LongNumbers/Program.cs
@@ -9,35 +9,10 @@ namespace LongNumbers
             => new Program().Execute();
         private void Execute()
         {
-<<<<<<< Updated upstream
-            Task1();
-        }
-        private void Task1()
-        {
-            while (true)
-            {
-                LongNumber longNumber1 = LongMath.GetLongNumber(ConsoleAsk("num1 :"));
-                LongNumber longNumber2 = LongMath.GetLongNumber(ConsoleAsk("num1 :"));
-                Console.WriteLine($"{longNumber1} + {longNumber2} = {longNumber1 + longNumber2}");
-                Console.WriteLine($"{longNumber1} - {longNumber2} = {longNumber1 - longNumber2}");
-                Console.WriteLine($"{longNumber1} * {longNumber2} = {longNumber1 * longNumber2}");
-                Console.WriteLine($"{longNumber1} / {longNumber2} = {longNumber1 / longNumber2}");
-                Console.WriteLine($"{longNumber1} % {longNumber2} = {longNumber1 % longNumber2}");
-                Console.WriteLine($"{longNumber1} % {longNumber2} - {longNumber2} = {longNumber1 % longNumber2 - longNumber2}");
-                Console.WriteLine($"{longNumber1} == {longNumber2} = {longNumber1 == longNumber2}");
-                Console.WriteLine($"{longNumber1} != {longNumber2} = {longNumber1 != longNumber2}");
-                Console.WriteLine($"{longNumber1} >= {longNumber2} = {longNumber1 >= longNumber2}");
-                Console.WriteLine($"{longNumber1} <= {longNumber2} = {longNumber1 <= longNumber2}");
-                Console.WriteLine($"{longNumber1} > {longNumber2} = {longNumber1 > longNumber2}");
-                Console.WriteLine($"{longNumber1} < {longNumber2} = {longNumber1 < longNumber2}");
-                Console.ReadKey();
-            }
-=======
             var num1 = LongMath.GetLongNumber("217783628520943495");
             var num2 = LongMath.GetLongNumber("232342340000002355");
             var answer = LongMath.GetLongNumber("-14 558 711 479 058 860");
             var a = num1 - num2;
->>>>>>> Stashed changes
         }
         private string ConsoleAsk(string question)
         {
